9f41dd1cb06a1300b3fb3a8311bc81935e99d47c
Firman Firdaus <firmanfirdaus542@gmail.com>
2024-08-21 15:12:47 +0700

    fitur prj

---
+++ application/views/prj/table-list.php
@@ -0,0 +1,13 @@
+<table id="prjTable" class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>PRJ</th>
+            <th>Date</th>
+            <th>Request By</th>
+            <th>Keterangan</th>
+            <th>State</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
